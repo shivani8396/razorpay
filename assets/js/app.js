bce231356e798cb9d7ee0b1c47c785c01ccba43a
Shivani Gupta <shivani@squareboat.com>
2018-10-11 19:06:13 +0530

    ADD form-heading number change on new form pane

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,26 @@
 
 $('.add-new').click(function() {
-
-// get the last DIV which ID starts with ^= "pane"
-var $formId= $('div[id^="pane"]:last');
-console.log($formId);
-
-// Read the Number from that DIV's ID
-// And increment that number by 1
-var num = parseInt( $formId.prop('id').match(/\d+/g), 10 ) +1;
-console.log(num);
-
-// Clone it and assign the new ID 
-var $pane = $formId.clone().prop('id', 'pane'+num);
-console.log($pane);
-
-// Finally insert pane in all after divs
-$formId.after( $pane.appendTo('.demo-form') );
-console.log('form added');
-
-// Change form heading number
-// $formId.after($('heading').clone().text('Person'+num+ 'Details'));
-// console.log('heading text changed');
+  
+  // get the last DIV which ID starts with ^= "pane"
+  var $formId= $('div[id^="pane"]:last');
+  console.log($formId);
+  
+  // Read the Number from that DIV's ID
+  // And increment that number by 1
+  var num = parseInt( $formId.prop('id').match(/\d+/g), 10 ) +1;
+  console.log(num);
+  
+  // Clone it and assign the new ID 
+  var $pane = $formId.clone().prop('id', 'pane'+num);
+  console.log($pane);
+  
+  // Finally insert pane in all after divs
+  $formId.after( $pane.appendTo('.demo-form') );
+  console.log('form added');
+  
+  // Change form heading number
+  $('.heading', $pane).text('Person '+ num+ ' Details');
+  // $formId.after($('.heading').text('Person'+num+ 'Details'));
+  console.log('heading text changed');
 });
 
